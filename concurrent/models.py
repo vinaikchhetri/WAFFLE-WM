eb9bb4a5e2d857301edf46bc53bf43295a66e2f9
vinaikchhetri <vinaikus@gmail.com>
2023-09-15 16:44:51 +0000

    copied existing work to concurrent folder.

---
+++ concurrent/models.py
@@ -0,0 +1,57 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class MP(nn.Module):
+    def __init__(self, in_dim, hid_dim, out_dim):
+        super().__init__()
+        self.hl1 = nn.Linear(in_dim, hid_dim)
+        nn.init.xavier_uniform_(self.hl1.weight)
+        nn.init.zeros_(self.hl1.bias)
+        self.hl2 = nn.Linear(hid_dim, out_dim)
+        nn.init.xavier_uniform_(self.hl2.weight)
+        nn.init.zeros_(self.hl2.bias)
+        self.ReLU = nn.ReLU()
+
+    def forward(self, x):
+        x = self.ReLU(self.hl1(x))
+        x = self.ReLU(self.hl2(x))
+        return x
+
+class CNN_MNIST(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(1, 32, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(32, 64, 5)
+        self.fc1 = nn.Linear(64 * 4 * 4, 512)
+        self.fc2 = nn.Linear(512, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, x.shape[1]*x.shape[2]*x.shape[3])
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+
+
+class CNN_CIFAR(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = torch.flatten(x, 1) # flatten all dimensions except batch
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
---
+++ concurrent/run.sh
@@ -0,0 +1,64 @@
+#!/bin/sh
+
+# #iid and mnist
+
+# #nn
+
+# #B=inf
+# python main.py --algo="FedAvg" --K=100 --C=0 --E=1 --B=8 --T=1500 --lr=0.01 --gpu="gpu" --model="nn" --name="exp_nn_inf_0" > ../logs/exp_nn_inf_0.txt
+# python main.py --algo="FedAvg" --K=100 --C=0.1 --E=1 --B=8 --T=1500 --lr=0.01 --gpu="gpu" --model="nn" --name="exp_nn_inf_0.1" > ../logs/exp_nn_inf_0.1.txt
+# python main.py --algo="FedAvg" --K=100 --C=0.2 --E=1 --B=8 --T=1500 --lr=0.01 --gpu="gpu" --model="nn" --name="exp_nn_inf_0.2" > ../logs/exp_nn_inf_0.2.txt
+
+# #B=10
+# python main.py --algo="FedAvg" --K=100 --C=0 --E=1 --B=10 --T=400 --lr=0.01 --gpu="gpu" --model="nn" --name="exp_nn_10_0" > ../logs/exp_nn_10_0.txt
+# python main.py --algo="FedAvg" --K=100 --C=0.1 --E=1 --B=10 --T=400 --lr=0.01 --gpu="gpu" --model="nn" --name="exp_nn_10_0.1" > ../logs/exp_nn_10_0.1.txt
+# python main.py --algo="FedAvg" --K=100 --C=0.2 --E=1 --B=10 --T=400 --lr=0.01 --gpu="gpu" --model="nn" --name="exp_nn_10_0.2" > ../logs/exp_nn_10_0.2.txt
+# python main.py --algo="FedAvg" --K=100 --C=0.5 --E=1 --B=10 --T=400 --lr=0.01 --gpu="gpu" --model="nn" --name="exp_nn_10_0.5" > ../logs/exp_nn_10_0.5.txt
+# python main.py --algo="FedAvg" --K=100 --C=1 --E=1 --B=10 --T=400 --lr=0.01 --gpu="gpu" --model="nn" --name="exp_nn_10_1" > ../logs/exp_nn_10_1.txt
+
+#cnn
+
+#B=inf
+#python main.py --algo="FedAvg" --K=100 --C=0 --E=5 --B=8 --T=400 --lr=0.01 --gpu="gpu" --model="cnn" --name="exp_cnn_inf_0" > ../logs/exp_cnn_inf_0.txt
+#python main.py --algo="FedAvg" --K=100 --C=0.1 --E=5 --B=8 --T=400 --lr=0.01 --gpu="gpu" --model="cnn" --name="exp_cnn_inf_0.1" > ../logs/exp_cnn_inf_0.1.txt
+#python main.py --algo="FedAvg" --K=100 --C=0.2 --E=5 --B=8 --T=400 --lr=0.01 --gpu="gpu" --model="cnn" --name="exp_cnn_inf_0.2" > ../logs/exp_cnn_inf_0.2.txt
+#python main.py --algo="FedAvg" --K=100 --C=0.5 --E=5 --B=8 --T=400 --lr=0.01 --gpu="gpu" --model="cnn" --name="exp_cnn_inf_0.5" > ../logs/exp_cnn_inf_0.5.txt
+#python main.py --algo="FedAvg" --K=100 --C=1 --E=5 --B=8 --T=400 --lr=0.01 --gpu="gpu" --model="cnn" --name="exp_cnn_inf_1" > ../logs/exp_cnn_inf_1.txt
+
+#B=10
+#python main.py --algo="FedAvg" --K=100 --C=0 --E=5 --B=10 --T=100 --lr=0.01 --gpu="gpu" --model="cnn" --name="exp_cnn_10_0" > ../logs/exp_cnn_10_0.txt
+#python main.py --algo="FedAvg" --K=100 --C=0.1 --E=5 --B=10 --T=100 --lr=0.01 --gpu="gpu" --model="cnn" --name="exp_cnn_10_0.1" > ../logs/exp_cnn_10_0.1.txt
+#python main.py --algo="FedAvg" --K=100 --C=0.2 --E=5 --B=10 --T=100 --lr=0.01 --gpu="gpu" --model="cnn" --name="exp_cnn_10_0.2" > ../logs/exp_cnn_10_0.2.txt
+#python main.py --algo="FedAvg" --K=100 --C=0.5 --E=5 --B=10 --T=100 --lr=0.01 --gpu="gpu" --model="cnn" --name="exp_cnn_10_0.5" > ../logs/exp_cnn_10_0.5.txt
+#python main.py --algo="FedAvg" --K=100 --C=1 --E=5 --B=10 --T=100 --lr=0.01 --gpu="gpu" --model="cnn" --name="exp_cnn_10_1" > ../logs/exp_cnn_10_1.txt
+
+# non-iid and mnist
+
+# nn
+
+# B=inf
+python main.py --algo="FedAvg" --K=100 --C=0 --E=1 --B=8 --T=5000 --lr=0.01 --gpu="gpu" --model="nn" --iid="false" --name="exp_nn_inf_0_f" > ../logs/exp_nn_inf_0_f.txt
+python main.py --algo="FedAvg" --K=100 --C=0.1 --E=1 --B=8 --T=2000 --lr=0.01 --gpu="gpu" --model="nn" --iid="false" --name="exp_nn_inf_0.1_f" > ../logs/exp_nn_inf_0.1_f.txt
+python main.py --algo="FedAvg" --K=100 --C=0.2 --E=1 --B=8 --T=2000 --lr=0.01 --gpu="gpu" --model="nn" --iid="false" --name="exp_nn_inf_0.2_f" > ../logs/exp_nn_inf_0.2_f.txt
+
+#B=10
+python main.py --algo="FedAvg" --K=100 --C=0 --E=1 --B=10 --T=4000 --lr=0.01 --gpu="gpu" --model="nn" --iid="false" --name="exp_nn_10_0_f" > ../logs/exp_nn_10_0_f.txt
+python main.py --algo="FedAvg" --K=100 --C=0.1 --E=1 --B=10 --T=800 --lr=0.01 --gpu="gpu" --model="nn" --iid="false" --name="exp_nn_10_0.1_f" > ../logs/exp_nn_10_0.1_f.txt
+python main.py --algo="FedAvg" --K=100 --C=0.2 --E=1 --B=10 --T=800 --lr=0.01 --gpu="gpu" --model="nn" --iid="false" --name="exp_nn_10_0.2_f" > ../logs/exp_nn_10_0.2_f.txt
+python main.py --algo="FedAvg" --K=100 --C=0.5 --E=1 --B=10 --T=600 --lr=0.01 --gpu="gpu" --model="nn" --iid="false" --name="exp_nn_10_0.5_f" > ../logs/exp_nn_10_0.5_f.txt
+python main.py --algo="FedAvg" --K=100 --C=1 --E=1 --B=10 --T=600 --lr=0.01 --gpu="gpu" --model="nn" --iid="false" --name="exp_nn_10_1_f" > ../logs/exp_nn_10_1_f.txt
+
+#cnn
+
+#B=inf
+python main.py --algo="FedAvg" --K=100 --C=0 --E=5 --B=8 --T=1500 --lr=0.01 --gpu="gpu" --model="cnn" --iid="false" --name="exp_cnn_inf_0_f" > ../logs/exp_cnn_inf_0_f.txt
+python main.py --algo="FedAvg" --K=100 --C=0.1 --E=5 --B=8 --T=1500 --lr=0.01 --gpu="gpu" --model="cnn" --iid="false" --name="exp_cnn_inf_0.1_f" > ../logs/exp_cnn_inf_0.1_f.txt
+python main.py --algo="FedAvg" --K=100 --C=0.2 --E=5 --B=8 --T=1500 --lr=0.01 --gpu="gpu" --model="cnn" --iid="false" --name="exp_cnn_inf_0.2_f" > ../logs/exp_cnn_inf_0.2_f.txt
+python main.py --algo="FedAvg" --K=100 --C=0.5 --E=5 --B=8 --T=1500 --lr=0.01 --gpu="gpu" --model="cnn" --iid="false" --name="exp_cnn_inf_0.5_f" > ../logs/exp_cnn_inf_0.5_f.txt
+
+#B=10
+python main.py --algo="FedAvg" --K=100 --C=0 --E=5 --B=10 --T=1500 --lr=0.01 --gpu="gpu" --model="cnn" --iid="false" --name="exp_cnn_10_0_f" > ../logs/exp_cnn_10_0_f.txt
+python main.py --algo="FedAvg" --K=100 --C=0.1 --E=5 --B=10 --T=500 --lr=0.01 --gpu="gpu" --model="cnn" --iid="false" --name="exp_cnn_10_0.1_f" > ../logs/exp_cnn_10_0.1_f.txt
+python main.py --algo="FedAvg" --K=100 --C=0.2 --E=5 --B=10 --T=500 --lr=0.01 --gpu="gpu" --model="cnn" --iid="false" --name="exp_cnn_10_0.2_f" > ../logs/exp_cnn_10_0.2_f.txt
+python main.py --algo="FedAvg" --K=100 --C=0.5 --E=5 --B=10 --T=500 --lr=0.01 --gpu="gpu" --model="cnn" --iid="false" --name="exp_cnn_10_0.5_f" > ../logs/exp_cnn_10_0.5_f.txt
+python main.py --algo="FedAvg" --K=100 --C=1 --E=5 --B=10 --T=500 --lr=0.01 --gpu="gpu" --model="cnn" --iid="false" --name="exp_cnn_10_1_f" > ../logs/exp_cnn_10_1_f.txt
