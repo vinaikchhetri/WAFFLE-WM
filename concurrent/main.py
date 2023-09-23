c88b00eed38455c29cf944f5d7a819ab5838ed0c
vinaikchhetri <vinaikus@gmail.com>
2023-09-23 17:14:03 +0000

    gpu>cpu.

--- concurrent/main.py
+++ concurrent/main.py
@@ -35,6 +35,9 @@ class CustomDataset(Dataset):
 		return img, label
 
 def client_update(client):
+   client.model_local.train()
+   client.model_local.to(client.device)
+   client.optimizer = optim.SGD(client.model_local.parameters(), lr=0.01, momentum=0.5)
    for epoch in range(client.args.E):
       running_loss = 0.0
       running_acc = 0.0
@@ -46,7 +49,9 @@ def client_update(client):
          inputs = inputs.to(client.device)
          labels = labels.to(client.device)
 
-         client.optimizer.zero_grad()
+         #client.optimizer.zero_grad()
+         for param in client.model_local.parameters():
+            param.grad = None
          if args.model == 'nn':
             inputs = inputs.flatten(1)
          outputs = client.model_local(inputs)
@@ -57,7 +62,8 @@ def client_update(client):
          #print(client_idx, time.time()-initial)
          client.optimizer.step()
 
-   return client.model_local.state_dict()
+   #return client.model_local.state_dict()
+   return client
 
 
 if __name__=='__main__':
@@ -105,7 +111,7 @@ if __name__=='__main__':
                available_indices = np.setdiff1d(available_indices, selected_indices)
 
          if args.gpu == "gpu":
-               device = torch.device('cuda:0')
+            device = torch.device('cuda')
          else:
             device = torch.device('cpu')
          
@@ -144,64 +150,70 @@ if __name__=='__main__':
             
             client_indices = np.random.choice(args.K, m, replace=False)
             #client_indices.astype(int)
-            num_samples_list = [num_samples_dict[idx] for idx in client_indices] #list containing number of samples for each user id.
-            total_num_samples = reduce(lambda x,y: x+y, num_samples_list, 0)
+            #num_samples_list = [num_samples_dict[idx] for idx in client_indices] #list containing number of samples for each user id.
+            #total_num_samples = reduce(lambda x,y: x+y, num_samples_list, 0)
             store = {}
 
-            with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+            with concurrent.futures.ThreadPoolExecutor(max_workers=min(len(client_indices), os.cpu_count() - 1)) as executor:
                results = []
                # Submit tasks to the executor
                for index,client_idx in enumerate(client_indices): #loop through selected clients
                  # pars = (trainset, index, client_idx, w_global.copy(), args, client_data_dict, criterion, model_local)
                   # initial = time.time()
-                  clients[client_idx].load_model(w_global)
-                  result = executor.submit(client_update, clients[client_idx])
+                  # clients[client_idx].load_model(w_global)
+                  clients[client_idx].load_model(model_global)
+                  results.append(executor.submit(client_update, clients[client_idx]).result())
                   #print(index," - ",time.time()-initial)
-                  results.append(result)
+                  
                # Retrieve results as they become available
-               for ind,future in enumerate(concurrent.futures.as_completed(results)):
-                  result = future.result()
-                  store[ind] = result         
-            #print("finished ", time.time() - initial)
-            w_global = {}
-            for layer in store[0]:
-               sum = 0
-               for user_key in store:
-                  sum += store[user_key][layer]*num_samples_list[user_key]/total_num_samples
-               w_global[layer] = sum
+               # for ind,future in enumerate(concurrent.futures.as_completed(results)):
+               #    result = future.result()
+               #    store[ind] = result  
+               a = concurrent.futures.as_completed(results)   
+               print(results)    
+
+            # #print("finished ", time.time() - initial)
+            # w_global = {}
+            # for layer in store[0]:
+            #    sum = 0
+            #    for user_key in store:
+            #       sum += store[user_key][layer]*num_samples_list[user_key]/total_num_samples
+            #    w_global[layer] = sum
             
 
-            #Performing evaluation on test data.
-            model_global.load_state_dict(w_global)
-            test_loader = torch.utils.data.DataLoader(testset, batch_size=64,
-                                       shuffle=False)
-            with torch.no_grad():
-               model_global.eval()
-               running_loss = 0.0
-               running_acc = 0.0
-               for index,data in enumerate(test_loader):  
-                  inputs, labels = data
-                  inputs = inputs.to(device)
-                  if args.model == 'nn':
-                     inputs = inputs.flatten(1)
-                  labels = labels.to(device)
-                  output = model_global(inputs)
-                  loss = criterion(output, labels)
-                  pred = torch.argmax(output, dim=1)
-                  acc = utils.accuracy(pred, labels)
-                  running_acc += acc
-                  running_loss += loss
-            avg_acc = running_acc / (index+1)
-            if best_test_acc<avg_acc:
-               best_test_acc = avg_acc
-               best_dict = {rounds: best_test_acc}
-               #torch.save(best_dict,'nn-trial.pt')
-            print('Round '+ str(rounds))
-            print(f'server stats: [loss: {running_loss / (index+1):.3f}')
-            print(f'server stats: [accuracy: {running_acc / (index+1):.3f}')
-            #test_acc.append(running_acc / (index+1))
-         # torch.save(test_acc,'../stats/'+args.name)
-         print("finished ", time.time() - initial)
+            # #Performing evaluation on test data.
+            # model_global.load_state_dict(w_global)
+            # print(rounds)
+
+         #    test_loader = torch.utils.data.DataLoader(testset, batch_size=64,
+         #                               shuffle=False)
+         #    with torch.no_grad():
+         #       model_global.eval()
+         #       running_loss = 0.0
+         #       running_acc = 0.0
+         #       for index,data in enumerate(test_loader):  
+         #          inputs, labels = data
+         #          inputs = inputs.to(device)
+         #          if args.model == 'nn':
+         #             inputs = inputs.flatten(1)
+         #          labels = labels.to(device)
+         #          output = model_global(inputs)
+         #          loss = criterion(output, labels)
+         #          pred = torch.argmax(output, dim=1)
+         #          acc = utils.accuracy(pred, labels)
+         #          running_acc += acc
+         #          running_loss += loss
+         #    avg_acc = running_acc / (index+1)
+         #    if best_test_acc<avg_acc:
+         #       best_test_acc = avg_acc
+         #       best_dict = {rounds: best_test_acc}
+         #       #torch.save(best_dict,'nn-trial.pt')
+         #    print('Round '+ str(rounds))
+         #    print(f'server stats: [loss: {running_loss / (index+1):.3f}')
+         #    print(f'server stats: [accuracy: {running_acc / (index+1):.3f}')
+         #    #test_acc.append(running_acc / (index+1))
+         # # torch.save(test_acc,'../stats/'+args.name)
+         # print("finished ", time.time() - initial)
 
 
 
